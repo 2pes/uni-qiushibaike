17bf862f86b1296c1d10bedf852ba185b2f665b0
amingccc <amingccc@gmail.com>
2020-09-11 23:19:34 +0800

    列表组件抽取

--- uni-qiushibaike/components/index/index-list.vue
+++ uni-qiushibaike/components/index/index-list.vue
@@ -1,15 +1,135 @@
 <template>
-	<div>
+	<view class="index-list">
+		<view class="index-list1 u-f-ac u-f-jsb">
+			<view class="u-f-ac">
+				<image :src="item.userpic" mode="widthFix" lazy-load></image>{{item.username}}
+			</view>
 
-	</div>
+			<view class="u-f-ac" v-show="item.isguanzhu">
+				<view class="icon iconfont icon-zengjia"></view>关注
+			</view>
+		</view>
+		<view class="index-list2">{{item.title}}</view>
+		<view class="index-list3 u-f-ajc">
+			<image :src="item.titlepic" mode="widthFix" lazy-load></image>
+			<!-- 视频 -->
+			<template v-if="item.type=='video'">
+				<view class="icon iconfont icon-zengjia index-list-play"></view>
+				<view class="index-list-playinfo">{{item.playnum}}万次播放 {{item.long}}</view>
+			</template>
+
+		</view>
+		<view class="index-list4 u-f-ac u-f-jsb">
+			<view class="u-f-ac">
+				<view class="u-f-ac " :class="{'active':(item.infonum.index==1)}">
+					<view class="icon iconfont icon-zengjia"></view>{{item.infonum.dingnum}}
+
+				</view>
+				<view class="u-f-ac" :class="{'active':(item.infonum.index==2)}">
+					<view class="icon iconfont icon-zengjia"></view>{{item.infonum.cainum}}
+				</view>
+			</view>
+			<view class="u-f-ac">
+				<view class="u-f-ac">
+					<view class="icon iconfont icon-pinglun"></view>{{item.commentnum}}
+
+				</view>
+				<view class="u-f-ac">
+					<view class="icon iconfont icon-zengjia"></view>{{item.sharenum}}
+				</view>
+			</view>
+		</view>
+	</view>
+
 </template>
 
 <script>
 	export default {
-		
+		props: {
+			item: Object,
+			index: Number
+		}
 	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+	.index-list {
+		padding: 20rpx;
+		border-bottom: 1rpx solid #EEEEEE;
 
+		.index-list1 {
+			view:first-child {
+				color: #999999;
+			}
+
+			view:first-child image {
+				width: 85rpx;
+				height: 85rpx;
+				border-radius: 100%;
+				margin-right: 10rpx;
+			}
+
+			view:last-child {
+				background: #F4F4F4;
+				border-radius: 2rpx;
+				padding: 0 10rpx;
+			}
+		}
+
+		.index-list2 {
+			padding-top: 15rpx;
+			font-size: 32rpx;
+		}
+
+		.index-list3 {
+			padding-top: 15rpx;
+			position: relative;
+
+			image {
+				width: 100%;
+				border-radius: 20rpx;
+			}
+
+			.index-list-play {
+				position: absolute;
+				font-size: 140rpx;
+				color: #FFFFFF;
+			}
+
+			.index-list-playinfo {
+				position: absolute;
+				background: rgba(51, 51, 51, 0.72);
+				bottom: 8rpx;
+				right: 8rpx;
+				border-radius: 40rpx;
+				color: #FFFFFF;
+				font-size: 22rpx;
+				padding: 0 10rpx;
+			}
+		}
+
+		.index-list4 {
+			padding: 15rpx 0;
+
+			.active {
+				view {
+					color: #0081FF;
+				}
+			}
+
+			view {
+				color: #999999;
+
+				view {
+
+					view,
+					view:first-child {
+						margin-right: 10rpx;
+					}
+
+
+				}
+			}
+		}
+	}
 </style>
--- uni-qiushibaike/pages/index/index.vue
+++ uni-qiushibaike/pages/index/index.vue
@@ -1,53 +1,17 @@
 <template>
 	<view class="container">
 		<block v-for="(item,index) in list" :key="index">
-			<view class="index-list">
-				<view class="index-list1 u-f-ac u-f-jsb">
-					<view class="u-f-ac">
-						<image :src="item.userpic" mode="widthFix" lazy-load></image>{{item.username}}
-					</view>
-
-					<view class="u-f-ac" v-show="item.isguanzhu">
-						<view class="icon iconfont icon-zengjia"></view>关注
-					</view>
-				</view>
-				<view class="index-list2">{{item.title}}</view>
-				<view class="index-list3 u-f-ajc">
-					<image :src="item.titlepic" mode="widthFix" lazy-load></image>
-					<!-- 视频 -->
-					<template v-if="item.type=='video'">
-						<view class="icon iconfont icon-zengjia index-list-play"></view>
-						<view class="index-list-playinfo">{{item.playnum}}万次播放 {{item.long}}</view>
-					</template>
-
-				</view>
-				<view class="index-list4 u-f-ac u-f-jsb">
-					<view class="u-f-ac">
-						<view class="u-f-ac " :class="{'active':(item.infonum.index==1)}">
-							<view class="icon iconfont icon-zengjia"></view>{{item.infonum.dingnum}}
-
-						</view>
-						<view class="u-f-ac" :class="{'active':(item.infonum.index==2)}">
-							<view class="icon iconfont icon-zengjia"></view>{{item.infonum.cainum}}
-						</view>
-					</view>
-					<view class="u-f-ac">
-						<view class="u-f-ac">
-							<view class="icon iconfont icon-pinglun"></view>{{item.commentnum}}
-
-						</view>
-						<view class="u-f-ac">
-							<view class="icon iconfont icon-zengjia"></view>{{item.sharenum}}
-						</view>
-					</view>
-				</view>
-			</view>
+			<index-list :item="item" :index="index"></index-list>
 		</block>
 	</view>
 </template>
 
 <script>
+	import indexList from "../../components/index/index-list.vue";
 	export default {
+		components: {
+			indexList
+		},
 		data() {
 			return {
 				list: [{
@@ -74,7 +38,7 @@
 					username: "昵称2", // 用户昵称
 					isguanzhu: true, // 是否关注
 					title: "我是标题", // 标题
-					type: "vedio", // img: 图文， video: 视频
+					type: "video", // img: 图文， video: 视频
 					titlepic: "../../static/demo/datapic/10.jpg", // 标题图片
 					playnum: "10w", // 播放次数
 					long: "3:47", // 视频时长
@@ -101,85 +65,5 @@
 		/* padding: 20px; */
 		font-size: 14px;
 		line-height: 24px;
-
-		.index-list {
-			padding: 20rpx;
-			border-bottom: 1rpx solid #EEEEEE;
-		}
-
-		.index-list1 {
-			view:first-child {
-				color: #999999;
-			}
-
-			view:first-child image {
-				width: 85rpx;
-				height: 85rpx;
-				border-radius: 100%;
-				margin-right: 10rpx;
-			}
-
-			view:last-child {
-				background: #F4F4F4;
-				border-radius: 2rpx;
-				padding: 0 10rpx;
-			}
-		}
-
-		.index-list2 {
-			padding-top: 15rpx;
-			font-size: 32rpx;
-		}
-
-		.index-list3 {
-			padding-top: 15rpx;
-			position: relative;
-
-			image {
-				width: 100%;
-				border-radius: 20rpx;
-			}
-
-			.index-list-play {
-				position: absolute;
-				font-size: 140rpx;
-				color: #FFFFFF;
-			}
-
-			.index-list-playinfo {
-				position: absolute;
-				background: rgba(51, 51, 51, 0.72);
-				bottom: 8rpx;
-				right: 8rpx;
-				border-radius: 40rpx;
-				color: #FFFFFF;
-				font-size: 22rpx;
-				padding: 0 10rpx;
-			}
-		}
-
-		.index-list4 {
-			padding: 15rpx 0;
-
-			.active {
-				view {
-					color: #0081FF;
-				}
-			}
-
-			view {
-				color: #999999;
-
-				view {
-
-					view,
-					view:first-child {
-						margin-right: 10rpx;
-					}
-
-
-				}
-			}
-		}
 	}
 </style>
