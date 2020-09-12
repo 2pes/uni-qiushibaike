749ec2f27a774bcf7ccda124db3cbda21a2528ae
amingccc <amingccc@gmail.com>
2020-09-12 10:53:28 +0800

    头部组件封装

---
+++ uni-qiushibaike/components/index/swiper-tab-head.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="uni-tab-bar">
+		<scroll-view scroll-x class="uni-swiper-tab">
+			<block v-for="(tab ,index) in tabBars" :key="tab.id">
+				<view class="swiper-tab-list" :class="{'active':tabIndex==index}" @tap="tabtap(index)">{{tab.name}}
+					<view class="swiper-tab-line"></view>
+				</view>
+			</block>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			tabBars: Array,
+			tabIndex: Number
+		},
+		methods: {
+			tabtap(index) {
+				this.$emit('tabtap', index);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.uni-tab-bar {
+		.uni-swiper-tab {
+			border-bottom: 1rpx solid #EEEEEE;
+
+			.swiper-tab-list {
+				color: #969696;
+				font-weight: bold;
+			}
+
+			.active {
+				color: #343434;
+
+				.swiper-tab-line {
+					border-bottom: 6rpx solid #fede33;
+					width: 70rpx;
+					margin: auto;
+					border-top: 6rpx solid #fede33;
+					border-radius: 20rpx;
+				}
+			}
+		}
+
+
+	}
+</style>
--- uni-qiushibaike/pages/index/index.vue
+++ uni-qiushibaike/pages/index/index.vue
@@ -1,32 +1,36 @@
 <template>
 	<view class="container">
+		<swiper-tab-head :tabBars="tabBars" :tabIndex="tabIndex" @tabtap="tabtap"></swiper-tab-head>
 		<view class="uni-tab-bar">
-			<scroll-view scroll-x class="uni-swiper-tab">
-				<block v-for="(tab ,index) in tabBars" :key="tab.id">
-					<view class="swiper-tab-list" :class="{'active':tabIndex==index}" @tap="tabtap(index)">{{tab.name}}
-						<view class="swiper-tab-line"></view>
-					</view>
-				</block>
-			</scroll-view>
+			<swiper class="swiper-box" :style="{height:swiperHeight+'px'}" :current="tabIndex" @change="tabChange">
+				<swiper-item v-for="(items,index) in newsList" :key="index">
+					<scroll-view scroll-y class="list">
+						<block v-for="(item,index1) in items.list" :key="index1">
+							<index-list :item="item" :index="index1"></index-list>
+						</block>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
 		</view>
-		<block v-for="(item,index) in list" :key="index">
-			<index-list :item="item" :index="index"></index-list>
-		</block>
 	</view>
 </template>
 
 <script>
 	import indexList from "../../components/index/index-list.vue";
+	import swiperTabHead from "../../components/index/swiper-tab-head.vue";
 	export default {
 		components: {
-			indexList
+			indexList,
+			swiperTabHead
 		},
 		data() {
 			return {
-				tabIndex: "0",
+				swiperHeight: 500,
+				tabIndex: 0,
 				tabBars: [{
 						name: "关注",
-						id: "1"
+						id: "1",
+						num:10
 					},
 					{
 						name: "推荐",
@@ -49,6 +53,85 @@
 						id: "6"
 					}
 				],
+				newsList: [{
+					list: [{
+
+						userpic: "../../static/demo/userpic/12.jpg", // 用户头像
+						username: "昵称", // 用户昵称
+						isguanzhu: false, // 是否关注
+						title: "我是标题", // 标题
+						type: "img", // img: 图文， video: 视频
+						titlepic: "../../static/demo/datapic/11.jpg", // 标题图片
+						playnum: "20w", // 播放次数
+						long: "2:47", // 视频时长
+						infonum: { // 点赞
+							index: 0, // 0：没有操作  1：点赞 2： 踩
+							dingnum: 11, // 点赞数量
+							cainum: 11, // 踩
+						},
+						commentnum: 0, // 评论
+						sharenum: 11, // 转发（分享）
+
+					}, {
+
+						userpic: "../../static/demo/userpic/11.jpg", // 用户头像
+						username: "昵称2", // 用户昵称
+						isguanzhu: true, // 是否关注
+						title: "我是标题", // 标题
+						type: "video", // img: 图文， video: 视频
+						titlepic: "../../static/demo/datapic/10.jpg", // 标题图片
+						playnum: "10w", // 播放次数
+						long: "3:47", // 视频时长
+						infonum: { // 点赞
+							index: 1, // 0：没有操作  1：点赞 2： 踩
+							dingnum: 11, // 点赞数量
+							cainum: 11, // 踩
+						},
+						commentnum: 0, // 评论
+						sharenum: 11, // 转发（分享）
+
+					}]
+				}, {
+					list: [{
+
+						userpic: "../../static/demo/userpic/11.jpg", // 用户头像
+						username: "昵称2", // 用户昵称
+						isguanzhu: true, // 是否关注
+						title: "我是标题", // 标题
+						type: "video", // img: 图文， video: 视频
+						titlepic: "../../static/demo/datapic/10.jpg", // 标题图片
+						playnum: "10w", // 播放次数
+						long: "3:47", // 视频时长
+						infonum: { // 点赞
+							index: 1, // 0：没有操作  1：点赞 2： 踩
+							dingnum: 11, // 点赞数量
+							cainum: 11, // 踩
+						},
+						commentnum: 0, // 评论
+						sharenum: 11, // 转发（分享）
+
+					}]
+				}, {
+					list: [{
+
+						userpic: "../../static/demo/userpic/12.jpg", // 用户头像
+						username: "昵称", // 用户昵称
+						isguanzhu: false, // 是否关注
+						title: "我是标题", // 标题
+						type: "img", // img: 图文， video: 视频
+						titlepic: "../../static/demo/datapic/11.jpg", // 标题图片
+						playnum: "20w", // 播放次数
+						long: "2:47", // 视频时长
+						infonum: { // 点赞
+							index: 0, // 0：没有操作  1：点赞 2： 踩
+							dingnum: 11, // 点赞数量
+							cainum: 11, // 踩
+						},
+						commentnum: 0, // 评论
+						sharenum: 11, // 转发（分享）
+
+					}]
+				}],
 				list: [{
 
 					userpic: "../../static/demo/userpic/12.jpg", // 用户头像
@@ -88,9 +171,20 @@
 				}]
 			}
 		},
+		onLoad() {
+			uni.getSystemInfo({
+				success: (res) => {
+					let height = res.windowHeight;
+					this.swiperHeight = height;
+				}
+			})
+		},
 		methods: {
 			tabtap(index) {
 				this.tabIndex = index;
+			},
+			tabChange(e) {
+				this.tabIndex = e.detail.current;
 			}
 		}
 
@@ -103,28 +197,6 @@
 		font-size: 14px;
 		line-height: 24px;
 
-		.uni-tab-bar {
-			.uni-swiper-tab {
-				border-bottom: 1rpx solid #EEEEEE;
 
-				.swiper-tab-list {
-					color: #969696;
-					font-weight: bold;
-				}
-
-				.active {
-					color: #343434;
-
-					.swiper-tab-line {
-						border-bottom: 6rpx solid #fede33;
-						width: 70rpx;
-						margin: auto;
-						border-top: 6rpx solid #fede33;
-						border-radius: 20rpx;
-					}
-				}
-			}
-
-		}
 	}
 </style>
