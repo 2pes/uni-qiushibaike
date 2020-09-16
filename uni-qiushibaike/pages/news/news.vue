9b9d29217176b10da47d9781efe76b434edc04d2
amingccc <amingccc@gmail.com>
2020-09-16 20:18:58 +0800

    公共列表组件

--- uni-qiushibaike/pages/news/news.vue
+++ uni-qiushibaike/pages/news/news.vue
@@ -1,5 +1,6 @@
 <template>
 	<view>
+		<!-- 头部导航 -->
 		<uni-nav-bar :statusBar="true" @clickRight="openAdd">
 			<!-- 左边 -->
 			<block slot="left">
@@ -24,6 +25,40 @@
 				</view>
 			</block>
 		</uni-nav-bar>
+		<!-- 列表 -->
+
+		<view class="common-list u-f">
+			<view class="common-list-l">
+				<image src="../../static/demo/userpic/10.jpg" mode="widthFix" lazy-load></image>
+			</view>
+			<view class="common-list-r">
+				<view class="u-f-ac u-f-jsb">
+					<view class="u-f-ac">昵称<view class="tag-sex icon iconfont icon-nan">25</view>
+					</view>
+					<view class="icon iconfont icon-zengjia">关注</view>
+				</view>
+				<view>我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题</view>
+				<view class="u-f-ajc">
+			<!-- 		<image src="../../static/demo/datapic/13.jpg" mode="widthFix" lazy-load></image>
+					<view class="icon iconfont icon-bofang index-list-play"></view>
+					<view class="index-list-playinfo">22w万次播放 23:23</view> -->
+					<view class="common-list-share u-f-ac">
+						<image src="../../static/demo/datapic/14.jpg" mode="widthFix" lazy-load></image>
+					<view>我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题</view>
+					</view>
+					
+				</view>
+				<view class="u-f-ac u-f-jsb">
+					<view>深圳 龙岗</view>
+					<view class="u-f u-f-jsb">
+						<view class="icon iconfont icon-zhuanfa">10</view>
+						<view class="icon iconfont icon-pinglun1">20</view>
+						<view class="icon iconfont icon-dianzan1">20</view>
+					</view>
+				</view>
+			</view>
+
+		</view>
 	</view>
 </template>
 
@@ -110,13 +145,95 @@
 	.common-list {
 		padding: 20rpx;
 
-		.common-list-1 {
-			flex-shrink: 0; //防止被压缩
+		.common-list-l {
+			flex-shrink: 0; //文字标题防止被压缩
 
 			image {
 				width: 90rpx;
 				height: 90rpx;
 				border-radius: 100%;
+			}
+		}
+
+		.common-list-r {
+			flex: 1;
+			margin-left: 15rpx;
+			border-bottom: 1rpx solid #EEEEEE;
+			padding-bottom: 10rpx;
+
+			>view:nth-child(3)>image {
+				width: 100%;
+				border-radius: 10rpx;
+			}
+
+			>view:nth-child(1)>view:first-child {
+				color: #999999;
+				font-size: 32rpx;
+			}
+
+			>view:nth-child(1)>view:last-child {
+				background: #EEEEEE;
+				padding: 0 10rpx;
+				font-size: 26rpx;
+			}
+
+			.tag-sex {
+				background: #007AFF;
+				color: #FFFFFF;
+				font-size: 23rpx;
+				padding: 5rpx 10rpx;
+				margin-left: 10rpx;
+				border-radius: 20rpx;
+				line-height: 22rpx;
+			}
+
+			>view:nth-child(2) {
+				font-size: 32rpx;
+				padding: 12rpx 0;
+			}
+
+			>view:nth-child(3) {
+				position: relative;
+
+				.index-list-play {
+					position: absolute;
+					font-size: 140rpx;
+					color: #FFFFFF;
+				}
+
+				.index-list-playinfo {
+					position: absolute;
+					background: rgba(51, 51, 51, 0.72);
+					bottom: 10rpx;
+					right: 10rpx;
+					border-radius: 40rpx;
+					color: #FFFFFF;
+					font-size: 22rpx;
+					padding: 0 10rpx;
+				}
+				.common-list-share{
+					background: #EEEEEE;
+					width: 100%;
+					padding: 10rpx;
+					border-radius: 10rpx;
+					>image{
+						width: 200rpx;
+						height: 150rpx;
+						margin-right: 10rpx;
+					}
+				}
+			}
+
+			>view:nth-child(4) {
+				>view {
+					color: #AAAAAA;
+				}
+
+
+				>view:nth-child(2)>view {
+					margin-left: 10rpx;
+					padding-left: 5rpx;
+				}
 			}
 		}
 	}
